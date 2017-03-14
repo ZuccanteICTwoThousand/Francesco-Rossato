85b8e62c1d03ee252a3b6490158dd4ca540c1bad
TheRoss8 <rossatino2000@gmail.com>
2017-03-14 16:30:13 +0100

    Re-added CashRegister

---
+++ IT/CashRegister/src/CashRegisterArr.java
@@ -0,0 +1,83 @@
+/**
+ * Created by Francesco on 07/03/2017.
+ */
+import java.util.ArrayList;
+
+/**
+ * Class to manage a cash register device
+ */
+public class CashRegisterArr {
+
+    private ArrayList <Double> shopper= new ArrayList <Double> ();
+    private double payment;
+
+    /**
+     * The standard constructor will initialize the payment variable at 0
+     */
+    public CashRegisterArr() {
+        payment=0;
+    }
+
+    /**
+     * Add to the bill an item's cost
+     * @param amount  The cost of an item
+     */
+    public void purchase(double amount){
+        shopper.add(amount);
+    }
+
+    /**
+     * Add to the bill a given number of items' cost
+     * @param amount The cost of an item
+     * @param howMany How many items of that kind the customer bought
+     */
+    public void purchase(double amount, double howMany){
+            shopper.add(amount*howMany);
+    }
+
+    /**
+     * Get the total
+     * @return the sum of the purchases made by the customer
+     */
+    public double getAmount(){
+        return this.sum();
+    }
+
+    /**
+     * Insert the customer payment
+     * @param payment The amount of money given by the customer
+     */
+    public void pay(double payment){
+        this.payment= payment;
+    }
+
+    /**
+     * Get the change and reset the bill count
+     * @return The change
+     */
+    public double giveChange(){
+        double change= payment-this.sum();
+        this.reset();
+        return change;
+    }
+
+    /**
+     * Get the items bought by the customer
+     * @return An array with all the items bought
+     */
+    public ArrayList getLog(){
+        return shopper;
+    }
+
+    private double sum(){
+        double out=0;
+        for(int i=0; i<shopper.size(); i++)
+            out+=shopper.get(i);
+        return out;
+    }
+
+    private void reset() {
+        this.shopper.clear();
+        this.payment=0;
+    }
+}
